ac8ccf249d899a0d666f01aab661f16d9d905da9
Jeff Frazier <jefff@knowclassic.com>
2015-11-23 23:31:28 -0500

    More themery and templates

--- client/views/articles/articles.js
+++ client/views/articles/articles.js
@@ -7,3 +7,65 @@ Template.Articles.helpers({
     return Articles.find();
   }
 });
+
+Template.Tabs.events({
+  "click .fav": function(e, t) {
+    var nid, node;
+    e.preventDefault();
+    nid = e.currentTarget.id;
+    console.log(nid);
+    node = Articles.findOne({
+      'nid': nid
+    });
+    // if (status.field_completed) {
+    //   return IonPopup.confirm({
+    //     title: 'Mark as incomplete?',
+    //     template: "This will reset completion date for " + objective.title,
+    //     onOk: function() {
+    //       return Meteor.call('toggleObjective', status._id);
+    //     },
+    //     onCancel: function() {}
+    //   });
+    // } else {
+      return IonPopup.confirm({
+        title: 'THE HOTNESS',
+        template: "Mark " + nid + " as the hotness?",
+        onOk: function() {
+          return "Marked as favorite"/*Meteor.call('toggleObjective', status._id);*/
+        },
+        onCancel: function() {}
+      });
+    // }
+  }
+});
+
+Template.Tabs.events({
+  "click .beer": function(e, t) {
+    var nid, node;
+    e.preventDefault();
+    nid = e.currentTarget.id;
+    console.log(nid);
+    node = Articles.findOne({
+      'nid': nid
+    });
+    // if (status.field_completed) {
+    //   return IonPopup.confirm({
+    //     title: 'Mark as incomplete?',
+    //     template: "This will reset completion date for " + objective.title,
+    //     onOk: function() {
+    //       return Meteor.call('toggleObjective', status._id);
+    //     },
+    //     onCancel: function() {}
+    //   });
+    // } else {
+      return IonPopup.confirm({
+        title: 'THIRSTY?',
+        template: "Yeah, it's time for a beer.",
+        onOk: function() {
+          return "Must be 5:00"/*Meteor.call('toggleObjective', status._id);*/
+        },
+        onCancel: function() {}
+      });
+    // }
+  }
+});
